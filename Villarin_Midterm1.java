aba36e9ddfcfdc567e3804937c5d061e7e8a72fd
Laurence Villarin <villarinalcantara166@gmail.com>
2026-05-07 23:53:23 +0800

    Villarin_Midterm1.java

--- Villarin_Midterm1.java
+++ Villarin_Midterm1.java
@@ -7,10 +7,10 @@ public class Villarin_Midterm1 {
         System.out.println("Name: Laurence Alfonso A. Villarin                 CYS: BCS14");
         System.out.println("===============================================================");
         System.out.println("Course Code   Title                         Time     Day   Room");
-        System.out.println("S-SCEN1BIP    ENVIRONMENTAL SCIENCE          7-10     TUE   JH1055");
-        System.out.println("S-ITCS121A    INTERMEDIATE PROGRAMMING LAB   7-10     TUE   ICT121");
-        System.out.println("S-ITCS111A    DISKURSO SA FILIPINO           7-8:30   TUE   ULS1");
-        System.out.println("D-REDB102     CHRISTIAN DISCIPLINE           1-2:30   TUE   JH1055");
-        System.out.println("D-ENGL101     ENGLISH FOR PURPOSEFUL COMMU   8:30-10  TUE   JH1055");
+        System.out.println("S-SCEN1BIP    ENVIRONMENTAL SCIENCE          7-10     TUESDAY   JH1055");
+        System.out.println("S-ITCS121A    INTERMEDIATE PROGRAMMING LAB   7-10     TUESDAY   ICT121");
+        System.out.println("S-ITCS111A    DISKURSO SA FILIPINO           7-8:30   TUESDAY   ULS1");
+        System.out.println("D-REDB102     CHRISTIAN DISCIPLINE           1-2:30   TUESDAY   JH1055");
+        System.out.println("D-ENGL101     ENGLISH FOR PURPOSEFUL COMMU   8:30-10  TUESDAY   JH1055");
     }
 }
